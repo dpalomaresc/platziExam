dd034752c0b4df0c04b751a1548895fddcabf204
Diego Palomares <diegopalomares@Diegos-MacBook-Pro.local>
2024-06-14 19:21:16 -0600

    init project

---
+++ platziExam/platziExam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  platziExam
+//
+//  Created by Diego Palomares on 14/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ platziExam/platziExam/platziExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  platziExamApp.swift
+//  platziExam
+//
+//  Created by Diego Palomares on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct platziExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
